806061efa83bb60f828f3ec2bd6010ac95cbd9aa
Mateus Alcantara <mateus.e.alcantara@outlook.com>
2023-03-15 09:57:05 -0300

    colocando animação ao clicar

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const burgerDeFacto = document.getElementsByClassName('burger')[1];
 burgerDeFacto.addEventListener('click', () => {
     burgerDeFacto.classList.toggle('active')
-    document.getElementsByClassName('ul-header')[0].classList.toggle('collapsible')
-    document.getElementsByTagName('input')[0].classList.toggle('collapsible')
+    document.getElementsByClassName('links-header')[0].classList.toggle('collapsible')
 })
